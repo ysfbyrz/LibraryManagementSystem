29ca66a0e0aff78f0ab6e65449e3eaede61096d9
Yusuf Boyraz <byrz.ysf@gmail.com>
2023-12-09 17:55:08 +0300

    Add project files.

---
+++ LibraryManagementSystem/HomePage.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace LibraryManagementSystem
+{
+    partial class HomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomePage));
+            this.HomePageMenu = new System.Windows.Forms.MenuStrip();
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bookSearchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.adminLoginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.studentLoginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bookSearchToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.HomePageMenu.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // HomePageMenu
+            // 
+            this.HomePageMenu.BackColor = System.Drawing.SystemColors.Control;
+            this.HomePageMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.HomePageMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeToolStripMenuItem,
+            this.bookSearchToolStripMenuItem,
+            this.bookSearchToolStripMenuItem1});
+            this.HomePageMenu.Location = new System.Drawing.Point(0, 0);
+            this.HomePageMenu.Name = "HomePageMenu";
+            this.HomePageMenu.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.HomePageMenu.Size = new System.Drawing.Size(1051, 28);
+            this.HomePageMenu.TabIndex = 0;
+            this.HomePageMenu.Text = "Menu";
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.homeToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("homeToolStripMenuItem.Image")));
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(88, 24);
+            this.homeToolStripMenuItem.Text = "Home ";
+            // 
+            // bookSearchToolStripMenuItem
+            // 
+            this.bookSearchToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.adminLoginToolStripMenuItem,
+            this.studentLoginToolStripMenuItem});
+            this.bookSearchToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("bookSearchToolStripMenuItem.Image")));
+            this.bookSearchToolStripMenuItem.Name = "bookSearchToolStripMenuItem";
+            this.bookSearchToolStripMenuItem.Size = new System.Drawing.Size(80, 24);
+            this.bookSearchToolStripMenuItem.Text = "Login";
+            // 
+            // adminLoginToolStripMenuItem
+            // 
+            this.adminLoginToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("adminLoginToolStripMenuItem.Image")));
+            this.adminLoginToolStripMenuItem.Name = "adminLoginToolStripMenuItem";
+            this.adminLoginToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.adminLoginToolStripMenuItem.Text = "Admin Login";
+            // 
+            // studentLoginToolStripMenuItem
+            // 
+            this.studentLoginToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("studentLoginToolStripMenuItem.Image")));
+            this.studentLoginToolStripMenuItem.Name = "studentLoginToolStripMenuItem";
+            this.studentLoginToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.studentLoginToolStripMenuItem.Text = "Student Login";
+            // 
+            // bookSearchToolStripMenuItem1
+            // 
+            this.bookSearchToolStripMenuItem1.Image = ((System.Drawing.Image)(resources.GetObject("bookSearchToolStripMenuItem1.Image")));
+            this.bookSearchToolStripMenuItem1.Name = "bookSearchToolStripMenuItem1";
+            this.bookSearchToolStripMenuItem1.Size = new System.Drawing.Size(125, 24);
+            this.bookSearchToolStripMenuItem1.Text = "Book Search";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(237, 161);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(576, 288);
+            this.panel1.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(156, 213);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(256, 41);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Closes at :  17 : 00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(156, 134);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(246, 41);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Opens at :  9 : 00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(229, 81);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 41);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "System";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(46, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(479, 41);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Welcome To Library Management";
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Red;
+            this.btnClose.Location = new System.Drawing.Point(1008, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(43, 40);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnClose_MouseClick);
+            // 
+            // HomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1051, 611);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.HomePageMenu);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.HomePageMenu;
+            this.Name = "HomePage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HomePage";
+            this.Load += new System.EventHandler(this.HomePage_Load);
+            this.HomePageMenu.ResumeLayout(false);
+            this.HomePageMenu.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip HomePageMenu;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bookSearchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem adminLoginToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem studentLoginToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bookSearchToolStripMenuItem1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
